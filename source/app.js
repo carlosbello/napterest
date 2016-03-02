14e27bd44b9a550cb4317ada3dc5d57b7791c679
Carlos Bello <bellopauste@yahoo.com>
2016-03-02 23:40:28 +0100

    List from factory method

--- source/app.js
+++ source/app.js
@@ -1,8 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var h1 = React.createElement('h1', { className: 'header' }, 'This is React');
-var p = React.createElement('p', { className: 'content', key: 'content' }, "And that's how it works.");
-var reactFragment = [h1, p];
-var section = React.createElement('section', { className: 'container' }, reactFragment);
 
-ReactDOM.render(section, document.getElementById('react-application'));
+var listItemElement1 = React.DOM.li({ className: 'item-1', key: 'item-1' }, 'Item 1');
+var listItemElement2 = React.DOM.li({ className: 'item-2', key: 'item-2' }, 'Item 2');
+var listItemElement3 = React.DOM.li({ className: 'item-3', key: 'item-3' }, 'Item 3');
+
+var reactFragment = [ listItemElement1, listItemElement2, listItemElement3 ];
+var listOfItems = React.DOM.ul({ className: 'list-of-items' }, reactFragment);
+
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
